d5a743752faf039d069fc2fc27dc0694ddb19c26
Aarav-07 <aaravparikh2006@gmail.com>
2025-08-25 18:37:20 +0530

    20-08-25

--- revision.py
+++ revision.py
@@ -8,3 +8,25 @@
                 #  2 2
                 #  3 3 3 
                 #  4 4 4 4
+t = (1, 2, 3)
+lst = list(t)
+lst.append(4)
+lst.append(5)
+print("Tuple to List + 2 elements:", lst)
+
+lst2 = [10, 20, 30]
+lst2.extend([40, 50, 60])
+print("After adding 3 elements:", lst2)
+print("Alternate elements:", lst2[::2])
+print("Reversed list:", lst2[::-1])
+
+n = input("Enter a number: ")
+n=int(n)
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+
+for i in range(1, n+1):
+    print(" " * (n - i), end="")
+    print("*" * (2*i - 1))
